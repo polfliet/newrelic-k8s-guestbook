7da727f99224f0e917ac074e5a9b93946d79c5cd
Stijn Polfliet <spolfliet@newrelic.com>
2019-03-14 07:33:57 -0700

    Wait for micro.k8s

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 microk8s.start
 echo 'Waiting for microk8s to start...'
-sleep 30
+sleep 5
+kubectl cluster-info
+echo 'Setting iptables...'
 sudo iptables -P FORWARD ACCEPT
+echo 'Ready for action!'
