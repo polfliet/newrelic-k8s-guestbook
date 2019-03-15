f6bd1858eb5fca50978f6a6184b4808bc803485e
Stijn Polfliet <spolfliet@newrelic.com>
2019-03-15 09:05:38 +0100

    Add loadgen arg check

--- loadgen.sh
+++ loadgen.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "Specify a port, for example: ./loadgen.sh 1234"
+    exit
+fi
+
 HOST="192.168.64.3"
 PORT=$1
   
 for i in {1..1000}
 do
         curl -d "message=automated_message_$i" -X POST http://$HOST:$PORT/message 
-        sleep 1
+        sleep 0.5
 done
